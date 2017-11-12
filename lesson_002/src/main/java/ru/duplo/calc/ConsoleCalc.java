85d2e363cf8c89cc3619cd5d6193b957ca115bf8
asdforia <asdf@epage.ru>
2017-11-12 08:42:45 +0300

    2017-11-12 morning from Forest

---
+++ lesson_002/src/main/java/ru/duplo/calc/ConsoleCalc.java
@@ -0,0 +1,18 @@
+package ru.duplo.calc;
+
+public class ConsoleCalc {
+    public static void main(String[] args){
+        final Calc calc = new Calc();
+        calc.calc(
+                args[1],
+                Double.valueOf(args[0]),
+                Double.valueOf(args[2])
+        );
+        System.out.println(String.format(
+                "%s %s %s = %s",
+                args[0], args[1], args[2],
+                calc.getResult()
+                )
+        );
+    }
+}
